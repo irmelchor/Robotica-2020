abf24c9955615e3a9d786262c0b3b5192c8b8265
irmelchor <irmelchor@alumnos.unex.es>
2020-09-26 13:31:26 +0200

    Ejemplo1 comentado

--- ejemplo1/ejemplo1.h
+++ ejemplo1/ejemplo1.h
@@ -16,9 +16,16 @@ class ejemplo1 : public QWidget, public Ui_Counter
         int elapsed = 1000;
 
     public slots:
+        //Método que hace la transición entre START y STOP del botón implementado.
         void doButton();
+
+        //Método que incrementa el valor mostrado en lcdNumber.
         void contador();
+
+        //Método que modifica la velocidad de lcdNumber siempre y cuando button se encuentre en START.
         void periodo();
+
+        //Método que cuenta el tiempo, en segundos, transcurrido desde el inicio del programa
         void elapsedTime();
 };
 
