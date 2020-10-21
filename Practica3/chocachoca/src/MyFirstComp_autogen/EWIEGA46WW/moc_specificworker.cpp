0b73f1f1c6efeaaf61b6a611ffc8d4df3e67ec0b
irmelchor <irmelchor@alumnos.unex.es>
2020-10-21 16:40:54 +0200

    Carpeta para practica3

---
+++ Practica3/chocachoca/src/MyFirstComp_autogen/EWIEGA46WW/moc_specificworker.cpp
@@ -0,0 +1,150 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'specificworker.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.8)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "specificworker.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'specificworker.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.12.8. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_SpecificWorker_t {
+    QByteArrayData data[16];
+    char stringdata0[152];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_SpecificWorker_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_SpecificWorker_t qt_meta_stringdata_SpecificWorker = {
+    {
+QT_MOC_LITERAL(0, 0, 14), // "SpecificWorker"
+QT_MOC_LITERAL(1, 15, 7), // "compute"
+QT_MOC_LITERAL(2, 23, 0), // ""
+QT_MOC_LITERAL(3, 24, 13), // "startup_check"
+QT_MOC_LITERAL(4, 38, 10), // "initialize"
+QT_MOC_LITERAL(5, 49, 6), // "period"
+QT_MOC_LITERAL(6, 56, 16), // "initializeMatrix"
+QT_MOC_LITERAL(7, 73, 11), // "checkMatrix"
+QT_MOC_LITERAL(8, 85, 1), // "x"
+QT_MOC_LITERAL(9, 87, 1), // "z"
+QT_MOC_LITERAL(10, 89, 5), // "alpha"
+QT_MOC_LITERAL(11, 95, 10), // "turnMethod"
+QT_MOC_LITERAL(12, 106, 25), // "RoboCompLaser::TLaserData"
+QT_MOC_LITERAL(13, 132, 5), // "ldata"
+QT_MOC_LITERAL(14, 138, 3), // "rot"
+QT_MOC_LITERAL(15, 142, 9) // "threshold"
+
+    },
+    "SpecificWorker\0compute\0\0startup_check\0"
+    "initialize\0period\0initializeMatrix\0"
+    "checkMatrix\0x\0z\0alpha\0turnMethod\0"
+    "RoboCompLaser::TLaserData\0ldata\0rot\0"
+    "threshold"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_SpecificWorker[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       6,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   44,    2, 0x0a /* Public */,
+       3,    0,   45,    2, 0x0a /* Public */,
+       4,    1,   46,    2, 0x0a /* Public */,
+       6,    0,   49,    2, 0x0a /* Public */,
+       7,    3,   50,    2, 0x0a /* Public */,
+      11,    3,   57,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Int,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int, QMetaType::Int, QMetaType::Float,    8,    9,   10,
+    QMetaType::Void, 0x80000000 | 12, QMetaType::Float, QMetaType::Float,   13,   14,   15,
+
+       0        // eod
+};
+
+void SpecificWorker::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<SpecificWorker *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->compute(); break;
+        case 1: { int _r = _t->startup_check();
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = std::move(_r); }  break;
+        case 2: _t->initialize((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 3: _t->initializeMatrix(); break;
+        case 4: _t->checkMatrix((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])),(*reinterpret_cast< float(*)>(_a[3]))); break;
+        case 5: _t->turnMethod((*reinterpret_cast< RoboCompLaser::TLaserData(*)>(_a[1])),(*reinterpret_cast< float(*)>(_a[2])),(*reinterpret_cast< float(*)>(_a[3]))); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject SpecificWorker::staticMetaObject = { {
+    &GenericWorker::staticMetaObject,
+    qt_meta_stringdata_SpecificWorker.data,
+    qt_meta_data_SpecificWorker,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *SpecificWorker::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *SpecificWorker::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_SpecificWorker.stringdata0))
+        return static_cast<void*>(this);
+    return GenericWorker::qt_metacast(_clname);
+}
+
+int SpecificWorker::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = GenericWorker::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 6)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 6;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 6)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 6;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
