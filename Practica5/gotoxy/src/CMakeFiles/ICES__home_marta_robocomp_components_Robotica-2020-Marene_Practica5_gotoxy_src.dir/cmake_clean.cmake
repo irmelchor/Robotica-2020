6d08bcc398dccaa417646dfb95e89d352ade0446
Marta1302 <martagt78@gmail.com>
2020-12-09 14:58:40 +0100

    9/12/2020

--- Practica5/gotoxy/src/CMakeFiles/ICES__home_marta_robocomp_components_Robotica-2020-Marene_Practica5_gotoxy_src.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ICES__home_marta_robocomp_components_Robotica-2020-Marene_Practica5_gotoxy_src.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
